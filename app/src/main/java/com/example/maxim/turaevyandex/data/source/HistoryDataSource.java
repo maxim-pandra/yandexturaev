464e5021a89676d683683978e437ebc57b1bef70
maxim <turaevmaxim@gmail.com>
2017-04-19 22:34:30 +0300

    initial commit

---
+++ app/src/main/java/com/example/maxim/turaevyandex/data/source/HistoryDataSource.java
@@ -0,0 +1,15 @@
+package com.example.maxim.turaevyandex.data.source;
+
+/**
+ * Created by maxim on 4/19/2017.
+ */
+
+public class HistoryDataSource {
+
+    interface GetBookmarksCallback {
+        void onBookmarksLoaded(List<Bookmark>);
+
+        void onDataNot
+
+    }
+}
